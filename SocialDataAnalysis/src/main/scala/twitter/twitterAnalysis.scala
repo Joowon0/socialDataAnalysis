944dea1e658266fa9ca0e80ce6abfb50a715b06c
ROK-LAPTOP\syndr <syndrome5044@naver.com>
2017-07-19 19:45:44 +0900

    src/main/resources/twitter 에 테스트데이터 올려줘

---
+++ SocialDataAnalysis/src/main/scala/twitter/twitterAnalysis.scala
@@ -0,0 +1,15 @@
+package twitter
+
+/**
+  * Created by syndr on 2017-07-19.
+  */
+
+case class twitterText() {
+
+}
+
+object twitterAnalysis {
+  def main(args: Array[String]) {
+    println("Empty Main")
+  }
+}
---
+++ SocialDataAnalysis/src/main/scala/twitter/twitterData.scala
@@ -0,0 +1,8 @@
+package twitter
+
+/**
+  * Created by syndr on 2017-07-19.
+  */
+object twitterData {
+
+}
