38c79ab1a1be16a71dde65c9a31f1cf370213472
Joowon <joowon7508@gmail.com>
2017-07-24 21:31:40 +0900

    more specifi

--- SocialDataAnalysis/src/main/scala/realTime/Post.scala
+++ SocialDataAnalysis/src/main/scala/realTime/Post.scala
@@ -2,18 +2,32 @@ package realTime
 
 import scala.collection.parallel.mutable.ParHashSet
 
-//-time
 
-// this should be splitted into empty and nonempty posts
 // would be more efficient to make a class with 3 posts with order
-class Post(timestamp: Timestamp) {
-  val totalScore = Var(0)
+trait Writing {
+  val totalScore : Int
+}
 
-  def update() : Unit = ???
+case class Post (timestamp: Timestamp) extends Writing {
+  val totalScore = Var(0)
+  def update() : Unit =  //problem here
+    totalScore() = comments map (_.totalScore) sum
+
+  def addComment(comment: Comment) : Unit =
+    comments + comment
 
   // a set of connected comments
-  val comments : ParHashSet[Long] = ParHashSet()
+  val comments : ParHashSet[Comment] = ParHashSet()
 }
+
+case class Comment ( commentID: Long, timestamp: Timestamp) extends Writing {
+  val totalScore = timestamp.score // this might be changed into Var()
+}
+
+case object Empty extends Writing {
+  val totalScore = 0
+}
+
 
 class Timestamp {
   val timestamp = ???
--- SocialDataAnalysis/src/main/scala/realTime/Query1.scala
+++ SocialDataAnalysis/src/main/scala/realTime/Query1.scala
@@ -2,14 +2,36 @@ package realTime
 
 import scala.collection.parallel.mutable.ParHashMap
 
-object Query1 {
-  object TOP3 {
-    val post1 : Post = ??? // this should be initiated into empty post
-    val post2 : Post = ???
-    val post3 : Post = ???
-  }
 
-  // given a comment ID, albe to find corresponding post
+class ThreePosts {
+  val post1 : Writing = Empty
+  val post2 : Writing = Empty
+  val post3 : Writing = Empty
+
+  // insert in sorted
+  def insert(post : Post): Unit = ???
+
+  // get top 3 posts that do not have score 0
+  def getTopPosts() =
+    post1 match {
+      case Empty => List()
+      case Post(_)  =>
+        post2 match {
+          case Empty   => List(post1)
+          case Post(_) =>
+            post3 match {
+              case Empty   => List(post2, post1)
+              case Post(_) => List(post3, post2, post1)
+            }
+      }
+    }
+}
+
+
+object Query1 {
+  val TOP3 = new ThreePosts
+
+  // given a comment ID, able to find corresponding post
   val connectedPost : ParHashMap[Long, Post] = ParHashMap()
 
   def findTop3(posts: List[Post]): List[Post] = ???
