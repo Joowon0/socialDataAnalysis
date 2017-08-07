516976a789d3303decae779ccb4d98efd3ec67a5
ROK-LAPTOP\syndr <syndrome5044@naver.com>
2017-08-08 01:45:50 +0900

    지금 컴파일이안됨 프로젝트다시만들어도 안되는데 내가 설정을 이상하게하는듯??? 누가 프로젝트설정 다시하고 코드만 옮겨줘...

--- SocialDataAnalysis/src/main/scala/twitter/twitterAnalysis.scala
+++ SocialDataAnalysis/src/main/scala/twitter/twitterAnalysis.scala
@@ -1,6 +1,7 @@
 package twitter
 
 import java.sql.Timestamp
+import org.apache.spark.SparkConf
 
 /**
   * Created by syndr on 2017-07-19.
@@ -14,8 +15,32 @@ case class CommentInfo(timestamp : Timestamp, comment_id : Long, user_id : Long,
   * */
 }
 
+case class FriendshipInfo(timestamp : Timestamp, user_id_1 : Long,
+                          user_id_2 : Long) {
+  /*
+  * 여기에 필요한 함수 정의
+   */
+}
+
+case class PostInfo(timestamp : Timestamp, post_id : Long,
+                    user_id : Long, post : String, user : String) {
+  /*
+  * 여기에 필요한 함수 정의
+   */
+}
+
+case class LikeInfo(timestamp : Timestamp, user_id : Long, comment_id : Long) {
+  /*
+  * 여기에 필요한 함수 정의
+   */
+}
+
+//case class dd() {}
+
 object twitterAnalysis {
-  def main(args: Array[String]) {
+  /*def main(args: Array[String]) {
     println("Empty Main")
-  }
+  }*/
+
+  val conf : SparkConf = new SparkConf().Set
 }
--- SocialDataAnalysis/src/main/scala/twitter/twitterData.scala
+++ SocialDataAnalysis/src/main/scala/twitter/twitterData.scala
@@ -12,3 +12,27 @@ object commentData {
     new File(resource.toURI).getPath
   }
 }
+
+object friendshipData {
+  private[friendship] def filePath = {
+    val resource = this.getClass.getClassLoader.getResource("twitter/friendships.dat")
+    if(resource == null) sys.error("friendships.dat == null")
+    new File(resource.toURI).getPath
+  }
+}
+
+object likeData {
+  private[like] def filePath = {
+    val resource = this.getClass.getClassLoader.getResource("twitter/like.dat")
+    if(resource == null) sys.error("like.dat == null")
+    new File(resource.toURI).getPath
+  }
+}
+
+object postData {
+  private[postt] def filePath = {
+    val resource = this.getClass.getClassLoader.getResource("twitter/post.dat")
+    if(resource == null) sys.error("post.dat == null")
+    new File(resource.toURI).getPath
+  }
+}
