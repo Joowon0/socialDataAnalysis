0b556d3fef8709ac411f444e7e85e3bc6f8f6592
Joowon <joowon7508@gmail.com>
2017-10-09 14:53:53 +0900

    Runable: changed into before

--- socialDataAnalysis/src/main/scala/wikipedia/WikipediaRanking.scala
+++ socialDataAnalysis/src/main/scala/wikipedia/WikipediaRanking.scala
@@ -9,68 +9,6 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
-case class CommentInfo(timestamp : Timestamp, comment_id : Long, user_id : Long,
-                       comment : String, user : String,
-                       comment_replied : Long, post_commented : Long) {
-  var score = 10
-  def getScore() : Int = score
-
-  def decrease(): Unit = {
-    score = score - 1
-  }
-
-  override def toString: String = {
-    val front =
-      score + "|" +
-        timestamp + "|" +
-        comment_id + "|" +
-        user_id + "|"
-    val middle =
-      if (comment != 0)
-        comment + "|" + user + "|"
-      else
-        "|"+ user + "|"
-    val back =
-      if (comment_replied != 0)
-        comment_replied + "|"
-      else
-        "|" + post_commented
-
-    front + middle + back
-  }
-}
-
-case class PostInfo(timestamp : Timestamp, post_id : Long,
-                    user_id : Long, post : String, user : String) {
-  var score = 10
-  def getScore() : Int = score
-
-  def decrease(): Unit = {
-    score = score -1
-  }
-
-  override def toString: String = {
-    val front =
-      score + "|" +
-        timestamp + "|" +
-        post_id + "|" +
-        user_id + "|"
-
-    val back =
-      if (post != 0)
-        post + "|" + user
-      else
-        "|" + user
-
-    front + back
-  }
-}
-
-case class LikeInfo(timestamp : Timestamp, user_id : Long, comment_id : Long) {}
-
-case class FriendshipInfo(timestamp : Timestamp, user_id_1 : Long,
-                          user_id_2 : Long) {}
-
 object WikipediaRanking {
   //val conf: SparkConf = new SparkConf().setMaster("spark://192.168.0.195:7077").setAppName("twitterAnalysis")
   val conf: SparkConf = new SparkConf().setMaster("local").setAppName("twitterAnalysis")
